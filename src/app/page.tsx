b1e3bbcfacab15374693fc147b4a61fb26675458
zeunig <business@mail.zeunig.hu>
2024-11-06 16:08:01 +0100

    push tuah commit on that thang

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,93 +3,33 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+<header className="aks">
+  <nav aria-label="Global" className="hb ma us zr zx asa dms">
+    <div className="ma dgl">
+      <a href="#" className="fv ars">
+        <span className="t">Your Company</span>
+        <img alt="" src="https://tailwindui.com/plus/img/logos/mark.svg?color=white" className="os ue"></img>
+      </a>
     </div>
+    <div className="ma ddw">
+      <button type="button" className="fx mb zr zw aet arw azr">
+        <span className="t">Open main menu</span>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon" className="oo sw">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"></path>
+        </svg>
+      </button>
+    </div>
+    <div className="mg dds diq">
+      <a href="#" className="axq ayc bbv">Product</a>
+      <a href="#" className="axq ayc bbv">Features</a>
+      <a href="#" className="axq ayc bbv">Marketplace</a>
+      <a href="#" className="axq ayc bbv">Company</a>
+    </div>
+    <div className="mg dds dgl dig">
+      <a href="#" className="axq ayc bbv">Log in <span aria-hidden="true">→</span>
+      </a>
+    </div>
+  </nav>
+</header>
   );
 }
