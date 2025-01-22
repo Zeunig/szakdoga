cd6398e161a957dd0be50fe7309899da37e37448
blucs <bojsza.bulcsu@gmail.com>
2025-01-22 22:02:19 +0100

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,12 @@ export function Header() {
         <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="/" className="flex items-center">
-<<<<<<< Updated upstream
-              <Image height={32} width={32} className="mr-3" src={"/logo.png"} alt="kpak logo"/>
-              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">KPAK</span>
-=======
+
+              
+
               <img src="/logo.png" className="mr-3 h-6 sm:h-9" alt="KPAK Logo" />
               <img src="/title.png" className="" alt="KPAK Title" />
->>>>>>> Stashed changes
+
             </a>
             <div className="flex items-center lg:order-2">
               <a href="auth" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in/Register</a>
