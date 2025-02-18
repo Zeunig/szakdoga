f3d5d44b33d9a8776cd42a1a74830b0f5201b641
blucs <bojsza.bulcsu@gmail.com>
2025-02-18 02:43:17 +0100

    Front end
    Profile page haladás

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,18 +1,137 @@
+
+
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
-import ProfileTabs from "@/components/ProfileTabs";
+import ProfileDataTable from "@/components/ProfileDataTable";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Tabs, TabsContent, TabsList, TabsTrigger, } from "@/components/ui/tabs"
+import { Cog } from "lucide-react";
 
-export default function Page(){
-    return(
-        <div >
-        <Header/> 
-        <div className="h-400 mb-60 w-50 bg-blue-400">
-            <div>
-                <ProfileTabs/>
+export default function Page() {
+    return (
+        <div>
+            <Header />
+            <div className="h-400 mb-60 w-50  mt-20">
+                <div>
+                    <div className="h-fit grid justify-items-center  mx-auto w-[1000px] max-w-screen-xl p-4 py-6 lg:py-8 border-2 border-gray-400 rounded-lg ">
+
+                        <Tabs defaultValue="cars" className="w-[700px] grid grid-cols-3 ">
+
+                            <TabsList className="col-1 -ml-24 w-fit h-[300px] grid grid-rows-4 border-2 border-gray-400 rounded-lg">
+                                <TabsTrigger value="profile" className="row-1 w-[130px]  border-2 border-gray-400 rounded-lg">Profil</TabsTrigger>
+                                <TabsTrigger value="settings" className="row-start-2 w-[130px] border-2 border-gray-400 rounded-lg">Regisztráció</TabsTrigger>
+                                <TabsTrigger value="cars" className="row-start-3 w-[130px] border-2 border-gray-400 rounded-lg">Feladott hírdetések</TabsTrigger>
+                                <TabsTrigger value="" className="row-start-4 w-[130px] border-2 border-gray-400 rounded-lg">Mentett hirdetések</TabsTrigger>
+                            </TabsList>
+
+
+                            {/*Profil*/}
+                            <TabsContent value="profile" className="-ml-40 -mr-24  mt-0  col-start-2 col-span-2">
+                                <Card className="h-[300px] border-2 border-gray-400 rounded-lg">
+
+                                    <CardHeader>
+                                        <CardTitle>Profil</CardTitle>
+                                        <CardDescription>
+                                            Itt tudod a profilod adatait megnézni és szerkeszteni
+                                            <hr className="w-[325Px] h-px bg-slate-400 border-0" />
+                                        </CardDescription>
+                                    </CardHeader>
+
+                                    <CardContent className="space-y-2">
+                                    <div className="space-y-1">
+                                            
+                                        </div>
+                                        <div className="space-y-1">
+                                            <Label htmlFor="email">Fiók név: </Label>
+                                            {"PLACEHOLDER"}
+                                        </div>
+                                        <div className="space-y-1">
+                                            <Label htmlFor="password">Email cím: </Label>
+                                            {"PLACEHOLDER"}
+                                        </div>
+                                    </CardContent>
+
+                                    <CardFooter>
+                                        
+                                    </CardFooter>
+
+                                </Card>
+                            </TabsContent>
+                            {/*Profil end*/}
+
+                            {/*Settings*/}
+                            <TabsContent value="settings" className="-ml-40 -mr-24  mt-0  col-start-2 col-span-2">
+                                <Card className="h-[300px] border-2 border-gray-400 rounded-lg">
+
+                                    <CardHeader>
+                                        <CardTitle> Beálítások</CardTitle>
+                                        <CardDescription>
+                                            Itt tudod a profilod adatait megnézni és szerkeszteni
+                                            <hr className="w-[325Px] h-px bg-slate-400 border-0" />
+                                        </CardDescription>
+                                    </CardHeader>
+
+                                    <CardContent className="space-y-2">
+                                    <div className="space-y-1">
+                                            
+                                        </div>
+                                        <div className="space-y-1">
+                                            <Label htmlFor="email">Fiók név: </Label>
+                                            {"PLACEHOLDER"}
+                                        </div>
+                                        <div className="space-y-1">
+                                            <Label htmlFor="password">Email cím: </Label>
+                                            {"PLACEHOLDER"}
+                                        </div>
+                                    </CardContent>
+
+                                    <CardFooter>
+                                        
+                                    </CardFooter>
+
+                                </Card>
+                            </TabsContent>
+                            {/*Profil end*/}
+
+                            {/*Profil*/}
+                            <TabsContent value="cars" className="-ml-40 -mr-24 mt-0  col-start-2 col-span-2">
+                                <Card className="border-2 h-[500px] border-gray-400 rounded-lg">
+
+                                    <CardHeader>
+                                        <CardTitle>Feladott hírdetések</CardTitle>
+                                        <CardDescription>
+                                            Itt látod és tudod szerkeszteni a feladott hídetéseid
+                                            <hr className="w-fit h-px bg-slate-400 border-0" />
+                                        </CardDescription>
+                                    </CardHeader>
+
+                                    <CardContent className="space-y-2">
+                                        <div className="space-y-1">
+                                            <ProfileDataTable/>
+                                        </div>
+                                    </CardContent>
+
+                                    <CardFooter>
+                                        
+                                    </CardFooter>
+
+                                </Card>
+                            </TabsContent>
+                            {/*Profil end*/}
+
+
+
+
+                        </Tabs>
+                    </div>
+                </div>
             </div>
+            <Footer />
         </div>
-        <Footer/>
-      </div>
-  
+
     )
 }
---
+++ src/components/ProfileDataTable.tsx
@@ -0,0 +1,88 @@
+import { Cog,Trash} from "lucide-react";
+import { Badge } from "./ui/badge";
+
+export default function ProfileDataTable(){
+    return(
+        <table id="filter-table">
+    <thead>
+        <tr>
+            <th>
+                <span className="flex items-center">
+                    Márka
+                    <svg className="w-8 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
+                    </svg>
+                </span>
+            </th>
+            <th>
+                <span className="flex items-center">
+                    Model
+                    <svg className="w-8 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
+                    </svg>
+                </span>
+            </th>
+            <th>
+                <span className="flex items-center">
+                    Ár
+                    <svg className="w-8 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
+                    </svg>
+                </span>
+            </th>
+            <th>
+                <span className="flex items-center">
+                    Akciós ár
+                    <svg className="w-8  h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
+                    </svg>
+                </span>
+            </th>
+            <th>
+                <span className="flex items-center">
+                    Státus
+                    <svg className="w-8 h-4 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m8 15 4 4 4-4m0-6-4-4-4 4"/>
+                    </svg>
+                </span>
+            </th>
+            <th>
+                <span className="flex items-center">
+                     Szerkesztés
+                </span>
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+
+        <tr className="border-2 border-gray-400">
+            <td className="font-medium text-gray-900 whitespace-nowrap "> {"Volkswagen"} </td>
+            <td> {"Golf"} </td>
+            <td> {"12 000 000"}Ft </td>
+            <td className="font-semibold text-cyan-700">{"10 000 000"}Ft </td>
+            <td> <Badge className="bg-orange-700">AKTÍV</Badge> </td>
+            <td> <button> <Cog/> </button> <button> <Trash/> </button> </td>
+        </tr>
+
+        <tr className="border-2 border-gray-400">
+            <td className="font-medium text-gray-900 whitespace-nowrap "> {"Volkswagen"} </td>
+            <td> {"Golf"} </td>
+            <td> {"12 000 000"}Ft </td>
+            <td className="font-semibold text-cyan-700">{"10 000 000"}Ft </td>
+            <td> <Badge className="bg-orange-700">AKTÍV</Badge> </td>
+            <td> <button> <Cog/> </button> <button> <Trash/> </button> </td>
+        </tr>
+
+        <tr className="border-2 border-gray-400">
+            <td className="font-medium text-gray-900 whitespace-nowrap "> {"Volkswagen"} </td>
+            <td> {"Golf"} </td>
+            <td> {"12 000 000"}Ft </td>
+            <td className="font-semibold text-cyan-700">{"10 000 000"}Ft </td>
+            <td> <Badge className="bg-orange-700">AKTÍV</Badge> </td>
+            <td> <button> <Cog/> </button> <button> <Trash/> </button> </td>
+        </tr>
+        
+    </tbody>
+</table>
+    )
+}
