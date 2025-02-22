bbb62b734f74266bdf972f47e10934462d7bac18
Bence <bence@zeunig.hu>
2025-02-23 00:11:12 +0100

    Car API with images

--- src/app/api/cars/[id]/route.ts
+++ src/app/api/cars/[id]/route.ts
@@ -6,19 +6,36 @@ import { NextRequest, NextResponse } from "next/server";
 // get car details by id and make it json-serializable
 async function hawkt(id: number) {
     const prisma = new PrismaClient({});
-    let query = await prisma.$queryRaw(Prisma.sql`
+    /*let query = await prisma.$queryRaw(Prisma.sql`
         SELECT szakdoga.car.*, szakdoga.user.name AS seller_name FROM szakdoga.car
         JOIN szakdoga.user ON szakdoga.car.seller_id=szakdoga.user.id
-        WHERE szakdoga.car.id = ${id}`);
-    query[0]["id"] = Number(query[0]["id"]);
-    query[0]["features"] = Number(query[0]["features"]);
+        WHERE szakdoga.car.id = ${id}`);*/
+    console.log(`Szám : ${id}`);
+    try {
+        var query = await prisma.car.findFirst({
+            where: {
+                id: {
+                    equals: Number(id) 
+                }
+            },
+            include: {
+                car_image_relation: true
+            }
+        });
+    }catch (err) {
+        console.log(err.stack);
+    }
+    
     console.log(query);
+    query["id"] = Number(query["id"]);
+    query["features"] = Number(query["features"]);
     return query;
 }
 
 export async function GET(req: NextRequest, context: {params: {id: number}}) { // ennél nagyobb retardáltságot rég nem írtam
+    const { id } = await context.params;
     const router = req.nextUrl.searchParams.get("id");
-    let hawk = await hawkt(context.params.id as unknown as number); 
+    let hawk = await hawkt(id as unknown as number); 
     let resp = NextResponse.json(hawk, {"status": 200});
     return resp;
 }
