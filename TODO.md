a6ca306dc5c518875cbd615c8994c478031eae63
blucs <bojsza.bulcsu@gmail.com>
2025-03-05 11:33:13 +0100

    Todo + fronend

---
+++ TODO.md
@@ -0,0 +1,34 @@
+# **Hátralévő dolgok**
+---
+##Frontend
+---
+-Landing
+-Keresés frontend
+-Responzivitás
+-/search undo
+
+-reklámok
+-auto kedvelés
+-fizetett kiemelés az uploadra
+
+
+##Backend
+---
+
+-header login meg a profile 
+-profilehoz my cars
+
+-auto kedvelés
+-email check hogy jó e
+
+-STRIPE
+
+
+
+##Fullstack
+---
+-my profile
+>logout
+>rendes adaok
+
+-jelszó min 8 karakter Aa1 formátum
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,25 @@ import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 
 export default function Home() {
-  
+
   return (
-    <div id="main">
-      <div className='mainimage'>
-        <Header/> 
-        <div className="container mx-auto mt-4 h-full flex justify-around flex-col ">
-          <h1 className="text-3xl md:text-6xl font-extrabold text-white text-center main-text-shadow">Körpörgő Autókereskedés</h1>
+    <div>
+      <Header />
+
+      <div className="container mx-auto mt-4 h-fit justify-around flex-col bg-[url(/mainbg.png)]  grid grid-rows-7">
+        <h1 className="row-start-3 text-3xl h-20 mt-2 md:text-6xl font-extrabold text-white text-center main-text-shadow">Körpörgő Autókereskedés</h1>
+
+        <div className="row-start-5 grid grid-cols-3"> 
+          <button className="col-start-1 bg-opacity-50 bg-gradient-to-r from-cyan-600 to-blue-700 hover:from-cyan-100 hover:to-slate-300 text-white hover:text-black border-2  rounded-lg transform transition duration-250 hover:scale-110  ">Sell</button>
+
+          <button className="col-start-3 border-2 border-slate-300 rounded-lg transform transition duration-250 hover:scale-110 hover:bg-blue-400">Purchase</button>
         </div>
-        <Footer/>
       </div>
+      <div>
+
+      </div>
+
+      <Footer />
     </div>
   )
 }
